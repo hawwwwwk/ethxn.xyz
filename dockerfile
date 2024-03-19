b84a55a1e708d97345233536ca9381a713d7f39e
hawwwwwk <ethanmahlke@gmail.com>
2024-03-18 20:04:34 -0700

    new dropdown!!!

--- dockerfile
+++ dockerfile
@@ -7,6 +7,11 @@ RUN rm /var/www/html/index.html
 
 COPY . /var/www/html
 
+COPY ./custom.conf /usr/local/apache2/conf/custom.conf
+
+RUN echo "Include /usr/local/apache2/conf/custom.conf" \
+    >> /usr/local/apache2/conf/httpd.conf
+
 EXPOSE 80
 
 CMD ["/usr/sbin/apache2ctl", "-D", "FOREGROUND"]
--- js/dropdown.js
+++ js/dropdown.js
@@ -1,25 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let dropdownButton = document.querySelector('.dropbtn');
+  let dropdownButtons = document.querySelectorAll('.dropbtn');
 
-  if(dropdownButton) {
-    dropdownButton.addEventListener('click', function() {
-      let dropdownContent = document.querySelector('.dropdown-content');
-      if(dropdownContent) {
-        dropdownContent.classList.toggle('show');
-      }
+  if(dropdownButtons) {
+    dropdownButtons.forEach(function(dropdownButton) {
+      dropdownButton.addEventListener('click', function() {
+        let dropdownContent = dropdownButton.nextElementSibling;
+        if(dropdownContent) {
+          dropdownContent.classList.toggle('show');
+        }
+      });
     });
   }
 
   // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
+  window.addEventListener('click', function(event) {
     if (!event.target.matches('.dropbtn')) {
-      let dropdowns = document.getElementsByClassName('dropdown-content');
-      for (let i = 0; i < dropdowns.length; i++) {
-        let openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
+      let dropdownContents = document.querySelectorAll('.dropdown-content');
+      dropdownContents.forEach(function(dropdownContent) {
+        if (dropdownContent.classList.contains('show')) {
+          dropdownContent.classList.remove('show');
         }
-      }
+      });
     }
-  };
+  });
 });
